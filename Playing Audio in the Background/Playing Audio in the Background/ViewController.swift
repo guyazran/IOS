b7de01aa5c88b94e8dd185fd90cb75967170a137
Guy Azran <guyazran@gmail.com>
2015-11-12 16:01:10 +0200

    Playing Audio in the Background

---
+++ Playing Audio in the Background/Playing Audio in the Background/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Playing Audio in the Background
+//
+//  Created by Guy Azran on 11/12/15.
+//  Copyright © 2015 Guy Azran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
