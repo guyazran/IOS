4cc2598a9037c23e0b5379d16932c59f31c46377
Guy Azran <guyazran@gmail.com>
2015-11-16 11:39:42 +0200

    Handling Location in the Background fix (Request Location Always)

--- Handling Location Changes in the Background/Handling Location Changes in the Background/AppDelegate.swift
+++ Handling Location Changes in the Background/Handling Location Changes in the Background/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         locationManager = CLLocationManager();
-        locationManager.requestWhenInUseAuthorization();
+        locationManager.requestAlwaysAuthorization();
         locationManager.desiredAccuracy = kCLLocationAccuracyBest;
         locationManager.delegate = self;
         locationManager.startUpdatingLocation();
