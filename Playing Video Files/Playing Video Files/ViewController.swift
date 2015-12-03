60a63d7f26a619b5ff1c1343eab6c6ea7cc1407f
Guy Azran <guyazran@gmail.com>
2015-12-03 14:29:07 +0200

    Playing Video Files

---
+++ Playing Video Files/Playing Video Files/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Playing Video Files
+//
+//  Created by Guy Azran on 12/3/15.
+//  Copyright © 2015 Guy Azran. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import MediaPlayer
+
+class ViewController: UIViewController, AVPlayerViewControllerDelegate {
+    
+    var moviePlayer: AVPlayerViewController?;
+    var playButton: UIButton!;
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        playButton = UIButton(type: .System);
+        playButton.frame = CGRect(x: 0, y: 0, width: 200, height: 30);
+        playButton.center = view.center;
+        playButton.setTitle("Play Video", forState: .Normal);
+        playButton.addTarget(self, action: "startPlayingVideo", forControlEvents: .TouchUpInside);
+        view.addSubview(playButton);
+    }
+    
+    func startPlayingVideo(){
+        moviePlayer = AVPlayerViewController(nibName: "", bundle: NSBundle.mainBundle());
+        moviePlayer!.delegate = self;
+        let url = NSBundle.mainBundle().URLForResource("bla", withExtension: ".mov");
+        moviePlayer!.player = AVPlayer(URL: url!);
+        presentViewController(moviePlayer!, animated: true, completion: nil);
+        moviePlayer!.player!.play();
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
