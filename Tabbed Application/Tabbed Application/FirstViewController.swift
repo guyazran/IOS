43ec9e734cbcbbef76da432f45d32d489cbd3eb0
Guy Azran <guyazran@gmail.com>
2015-12-07 15:11:11 +0200

    Tabbed Application

---
+++ Tabbed Application/Tabbed Application/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tabbed Application
+//
+//  Created by Guy Azran on 12/7/15.
+//  Copyright © 2015 Guy Azran. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Tabbed Application/Tabbed Application/ThirdViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ThirdViewController.swift
+//  Tabbed Application
+//
+//  Created by Guy Azran on 12/7/15.
+//  Copyright © 2015 Guy Azran. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController:  UIViewController{
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
