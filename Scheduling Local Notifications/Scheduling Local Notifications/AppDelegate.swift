af92c8c8248161d2a1e85d8fe9a0867085ba49ed
Guy Azran <guyazran@gmail.com>
2015-12-10 09:21:18 +0200

    Scheduling Local Notifications

---
+++ Scheduling Local Notifications/Scheduling Local Notifications/AppDelegate.swift
@@ -0,0 +1,82 @@
+//
+//  AppDelegate.swift
+//  Scheduling Local Notifications
+//
+//  Created by Guy Azran on 12/7/15.
+//  Copyright © 2015 Guy Azran. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        if let options = launchOptions{
+            let value = options[UIApplicationLaunchOptionsLocalNotificationKey] as? UILocalNotification;
+            if let notification = value{
+                application
+            }
+        }
+        
+        //get user permission for performing local notifications
+        let settings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil);
+        application.registerUserNotificationSettings(settings);
+        
+        return true
+    }
+    
+    
+    
+    func application(application: UIApplication, didRegisterUserNotificationSettings notificationSettings: UIUserNotificationSettings) {
+        
+        if notificationSettings.types.isEmpty{ //the user did not allow any type of notification
+            return;
+        }
+        
+        let notification = UILocalNotification();
+        notification.fireDate = NSDate(timeIntervalSinceNow: 5);
+        notification.timeZone = NSCalendar.currentCalendar().timeZone;
+        notification.alertBody = "you have a new message!";
+        notification.hasAction = true;
+        notification.alertAction = "show";
+        notification.applicationIconBadgeNumber++;
+        notification.soundName = "any string";
+        notification.userInfo = [
+            "key 1" : "Value 1",
+            "key 2" : "Value 2"
+        ];
+        application.scheduleLocalNotification(notification);
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
